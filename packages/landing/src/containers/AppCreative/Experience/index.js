def594d3df9dde188c9abbce9421459967d432d0
lamtheram <68121067+lamtheram@users.noreply.github.com>
2021-07-13 18:51:02 +0100

    Update index.js

--- packages/landing/src/containers/AppCreative/Experience/index.js
+++ packages/landing/src/containers/AppCreative/Experience/index.js
@@ -22,88 +22,14 @@ import SectionWrapper, {
 } from './experience.style';
 import { experiences } from 'common/data/AppCreative';
 
-// close button for modal
-const CloseModalButton = () => (
-  <Button
-    className="modalCloseBtn"
-    variant="fab"
-    onClick={() => closeModal()}
-    icon={<i className="flaticon-plus-symbol" />}
-  />
-);
-
-const ModalContent = () => (
-  <VideoWrapper>
-    <iframe
-      title="Video"
-      src="https://www.youtube.com/embed/hW98BFnVCm8"
-      frameBorder="0"
-    />
-  </VideoWrapper>
-);
-
 const Experiences = () => {
   const { slogan, title, features, video_theme, clients } = experiences;
-
-  // modal handler
-  const handleVideoModal = () => {
-    openModal({
-      config: {
-        className: 'video-modal',
-        disableDragging: true,
-        default: {
-          width: 'auto',
-          height: 'auto',
-          x: 0,
-          y: 0,
-        },
-      },
-      component: ModalContent,
-      componentProps: {},
-      closeComponent: CloseModalButton,
-      closeOnClickOutside: true,
-    });
-  };
-
   return (
     <SectionWrapper id="experience">
       <Container>
-        <SectionHeader>
-          <Heading content={title} />
-          <Text content={slogan} />
-        </SectionHeader>
-        <ExperienceMainWrap>
-          <VideoArea onClick={handleVideoModal}>
-            <img src={video_theme} alt="Microsoft" />
-            <Button
-              className="video__btn"
-              icon={<Icon className="plus" icon={ic_play_arrow} />}
-            />
-          </VideoArea>
-          <ExperienceWrapper>
-            {features.map((item, index) => (
-              <FeatureBlock
-                key={`post_key-${index}`}
-                id={`post_id-${item.id}`}
-                className="experience__item"
-                icon={
-                  <Image
-                    src={item.icon}
-                    alt={`Icon ${item.id}`}
-                    objectFit="cover"
-                    className="experience__image"
-                  />
-                }
-                iconPosition="left"
-                title={<Heading as="h4" content={item.title} />}
-                description={<Text content={item.description} />}
-              />
-            ))}
-          </ExperienceWrapper>
-        </ExperienceMainWrap>
         <ClientWrapper>
           <div className="client__text">
-            <Text as="span" content={'Companies who worked with us proudly'} />
+            <Text as="span" content={'Partner companies and programs'} />
           </div>
           <ImageSlider>
             <ImageSlide>
